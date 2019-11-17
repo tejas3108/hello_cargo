ba8d6077f151d7ed2acd44b626fbff5857086d20
Tejas Deshpande <deshpandetejas95@gmail.com>
2019-11-17 15:15:42 -0600

    Basic operations in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let x= 43 / 5;
+    println!("The value of x is {}", x);
+
+    //now trying to initialise an array using the value of x
+    let arr = vec![0;x];
+    println!("The size of arr is {}", arr.len());
+
+    //call helper method
+    println!("{}", sample_printer(3,5));
+}
+
+fn sample_printer(num1:u32, num2:u32) -> u32{
+    num1 + num2
 }
